3a13a498a1ac9f4c7e8a9844333bc9e1ab5d7174
jean.laneurit <jean.laneurit@irstea.fr>
2020-09-29 12:38:27 +0200

    add function to extract duration from msg

--- romea_common_utils/include/romea_common_utils/conversions/TimeConversions.hpp
+++ romea_common_utils/include/romea_common_utils/conversions/TimeConversions.hpp
@@ -1,17 +1,31 @@
 #ifndef _romea_TimeConversion_hpp_
-#define _romea_TimeConversion_hpp
+#define _romea_TimeConversion_hpp_
 
 //romea
 #include <romea_common/time/Time.hpp>
 
 //ros
-#include <ros/time.h>
+#include <ros/ros.h>
 
 namespace romea {
 
    ros::Time toROSTime(const romea::Duration & duration);
 
    romea::Duration toRomeaDuration(const ros::Time & time);
+
+   template <typename Msg>
+   Duration extractDuration(const Msg & msg)
+   {
+     auto time_stamp = ros::message_traits::timeStamp(msg);
+     if(time_stamp)
+     {
+       return toRomeaDuration(*time_stamp);
+     }
+     else
+     {
+       return toRomeaDuration(ros::Time::now());
+     }
+   }
 }
 
 #endif
